63af36b71743095d8901073ff3e2fa76947fec99
gerson dos santos de souza <gersonsouza97@outlook.com>
2021-07-04 23:51:23 -0300

    exibir lista de livros

--- src/main/java/br/com/zupacademy/gerson/casadocodigo/controller/LivroController.java
+++ src/main/java/br/com/zupacademy/gerson/casadocodigo/controller/LivroController.java
@@ -1,16 +1,20 @@
 package br.com.zupacademy.gerson.casadocodigo.controller;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.zupacademy.gerson.casadocodigo.dto.ListaLivros;
 import br.com.zupacademy.gerson.casadocodigo.dto.LivroDto;
 import br.com.zupacademy.gerson.casadocodigo.model.Livro;
 
@@ -27,5 +31,12 @@ public class LivroController {
 		Livro livro = request.toLivro(em);
 		em.persist(livro);
 		return ResponseEntity.ok().body(livro);
+	}
+	
+	@GetMapping("/listaLivros")
+	public ResponseEntity<?> listaLivros(){
+		List<ListaLivros> lista = ListaLivros.toLista(em);
+		
+		return ResponseEntity.ok().body(lista);
 	}
 }
---
+++ src/main/java/br/com/zupacademy/gerson/casadocodigo/dto/ListaLivros.java
@@ -0,0 +1,45 @@
+package br.com.zupacademy.gerson.casadocodigo.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import br.com.zupacademy.gerson.casadocodigo.model.Livro;
+
+public class ListaLivros {
+
+	private Long id;
+	private String titulo;
+
+	public ListaLivros(Long id, String titulo) {
+		super();
+		this.id = id;
+		this.titulo = titulo;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public static List<ListaLivros> toLista(EntityManager em) {
+		TypedQuery<Livro> query = em.createQuery("SELECT l FROM Livro l", Livro.class);
+		List<Livro> listaResultado = query.getResultList();
+
+	//	List<ListaLivros> listaLivros = new ArrayList<ListaLivros>();
+	//	listaResultado.forEach(livro -> {
+	//		listaLivros.add(new ListaLivros(livro.getId(), livro.getTitulo()));
+	//	});
+
+	//	return listaLivros;
+		return listaResultado.stream().map(livro -> new ListaLivros(livro.getId(), livro.getTitulo())).collect(Collectors.toList());
+
+	}
+
+}
